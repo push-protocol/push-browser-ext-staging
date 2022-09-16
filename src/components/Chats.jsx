7c682a5afb2339f5a578e6d381f301ab48926790
corlard3y <corlardey@gmail.com>
2022-09-16 15:03:16 +0100

    chat container

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -14,6 +14,35 @@ const Chats = () => {
                  <FiSearch size={18} style={{ color: '#657795' }} />
             </SearchIcon>
         </SearchBox>
+
+
+        <ChatContainer>
+            <Blocky></Blocky>
+            <Details>
+                <b>Adam.eth</b>
+                <span>Hey are you there?</span>
+            </Details>
+
+            <Time>
+                <span>9:30</span>
+                {/* <span>Time</span> */}
+            </Time>
+        </ChatContainer>
+
+
+        <ChatContainer>
+            <Blocky></Blocky>
+            <Details>
+                <b>Adam.eth</b>
+                <span>Hey are you there?</span>
+            </Details>
+
+            <Time>
+                <span>9:30</span>
+                {/* <span>Time</span> */}
+            </Time>
+        </ChatContainer>
+
         </Item>
     </Section>
   )
@@ -29,6 +58,7 @@ const SearchBox = styled.div`
   border-radius:99px;
   padding: 0px 10px;
   margin-top: 20px;
+  margin-bottom: 15px;
   &:focus-within {
     outline: none;
     background-image: linear-gradient(#f4f5fa, #f4f5fa), linear-gradient(to right, #cf1c84, #8ed6ff);
@@ -54,6 +84,65 @@ const Inputbar = styled.input`
         font-family: strawford;
     }
 `
+
+const ChatContainer = styled.div`
+  width:320px;
+  display: flex;
+  flex-direction: row;
+  align-items:center;
+  height:50px;
+  margin: 10px 0;
+`
+
+const Blocky = styled.div`
+    width:48px;
+    height:48px;
+    background: rgb(34,193,195);
+    background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
+    border-radius: 99px;
+`
+
+const Details = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left:10px;
+    b {
+        font-style: normal;
+        font-weight: 500;
+        font-size: 17px;
+        letter-spacing: -0.019em;
+        color: #1E1E1E;
+    }
+    span {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 130%;
+        color: #657795;
+    }
+`
+const Time = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left:auto;
+    align-self:flex-start;
+    span {
+        font-family: 'Strawford';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 15px;
+        line-height: 150%;
+        color: #657795;
+    }
+    span {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 130%;
+        color: #657795;
+    }
+`
+
 
 
 
