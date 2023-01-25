b7926f910daf7e11ca509cd0092056a2513e6b73
corlard3y <corlardey@gmail.com>
2023-01-25 18:24:50 +0100

    add brave function

--- src/App.js
+++ src/App.js
@@ -12,6 +12,43 @@ import NotificationPage from "./pages/NotificationPage/NotificationPage";
 import Home from "./pages/HomePage/HomePage";
 
 console.log = function () {};
+
+function fnBrowserDetect() {
+  let userAgent = navigator.userAgent;
+  let browserName;
+
+  if (userAgent.match(/chrome|chromium|crios/i)) {
+    browserName = "chrome";
+  } else if (userAgent.match(/firefox|fxios/i)) {
+    browserName = "firefox";
+  } else if (userAgent.match(/safari/i)) {
+    browserName = "safari";
+  } else if (userAgent.match(/opr\//i)) {
+    browserName = "opera";
+  } else if (userAgent.match(/edg/i)) {
+    browserName = "edge";
+  } else {
+    browserName = "No browser detection";
+  }
+
+  chrome.extension.getBackgroundPage().console.log(browserName);
+
+  // document.querySelector("h1").innerText =
+  //   "You are using " + browserName + " browser";
+}
+
+function isBrave() {
+  if (window.navigator.brave != undefined) {
+    if (window.navigator.brave.isBrave.name == "isBrave") {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
 function App() {
   const [registered, setRegistered] = useState(false);
   const [walletAddr, setWalletAddr] = useState(null);
@@ -26,6 +63,9 @@ function App() {
       }
     });
   });
+  fnBrowserDetect();
+  isBrave();
+  chrome.extension.getBackgroundPage().console.log(isBrave());
   if (!registered)
     return (
       <NotifsContext.Provider value={[notifs, setNotifs]}>
